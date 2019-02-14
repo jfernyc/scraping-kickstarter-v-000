dca245c745c5de3f0fca7e01f0b60f917c07ce23
J Fern <jfern20@gmail.com>
2019-02-14 03:21:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,8 +21,8 @@ projects[title.to_sym] = {
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  
 }   
+
 end 
 projects 
 
